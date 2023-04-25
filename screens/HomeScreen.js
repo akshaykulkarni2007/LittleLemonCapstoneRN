f584469afee42193c0dff5eb948202c193641765
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-04-25 15:14:37 +0530

    started menu sqlite

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,67 @@
 import { useState, useEffect } from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+import * as SQLite from 'expo-sqlite'
 
 import { Header, MenuItem } from '../components'
+
+const db = SQLite.openDatabase('little_lemon')
 
 export const HomeScreen = () => {
 	const [menu, setMenu] = useState([])
 
 	useEffect(() => {
-		getMenu()
+		db.transaction((tx) => {
+			tx.executeSql(
+				'CREATE TABLE IF NOT EXISTS menu (name TEXT, description TEXT, price TEXT, image TEXT, category TEXT)'
+			)
+		})
+
+		fetchMenu()
+
+		db.transaction((tx) => {
+			tx.executeSql('DROP TABLE menu', (txObj, resultSet) => {})
+		})
 	}, [])
 
-	const getMenu = async () => {
+	const fetchMenu = async () => {
+		db.transaction(async (tx) => {
+			tx.executeSql(
+				'SELECT * FROM menu',
+				null,
+				async (txObj, { rows: { _array } }) => {
+					const dbMenu = _array
+
+					if (dbMenu.length === 0) {
+						const APIMenu = await fetchMenuFromAPI()
+						setMenu(APIMenu)
+
+						db.transaction((tx) => {
+							console.log('inserting')
+
+							tx.executeSql(
+								'INSERT INTO menu (name, description, price, image, category) values ?',
+								[],
+								(txObj, resultSet) => console.log('inserted', resultSet),
+								(txObj, error) => console.log('Error', error)
+							)
+						})
+					} else {
+						console.log('found')
+						setMenu(dbMenu)
+					}
+				},
+				(txObj, error) => console.log('Error ', error)
+			)
+		})
+	}
+
+	const fetchMenuFromAPI = async () => {
 		const res = await fetch(
 			'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json'
 		)
 		const data = await res.json()
-		setMenu(data.menu)
+		return data.menu
 	}
 
 	return (
